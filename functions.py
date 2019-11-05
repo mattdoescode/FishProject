369b515f202548598146d2e57a399e61f2817097
Matthew Loewen <matthewlanimates@gmail.com>
2019-11-05 00:22:08 -0500

    fish movement visualization from CSV

--- functions.py
+++ functions.py
@@ -1,16 +1,19 @@
 import csv
 import pygame
 
+
 def writeToCSV(data):
     # write a new csv file
     with open('fishData.csv', 'w', newline='') as csvFile:
         fileWriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         fileWriter.writerow(data)
 
+
 def appendToCSV(data):
     with open('fishData.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(data)
+
 
 def averageCSV():
     print("attempting to correct CSV file")
@@ -25,28 +28,34 @@ def averageCSV():
 
     with open('fishDataCorrected.csv', 'w', newline='') as csvFile:
         fileWriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        fileWriter.writerow(["object-number", "recorded-time", "run-time", "frame-number", "x-position", "y-position", "z-position"])
+        fileWriter.writerow(
+            ["object-number", "recorded-time", "run-time", "frame-number", "x-position", "y-position", "z-position"])
         for i in new_rows_list:
             fileWriter.writerow(i.values())
 
     print("completed. new csv file")
 
     # to average the CSV
-        # check difference in times of frame x and x+1
-        # if time difference is greater than 0.15 seconds
-        # make a new record for every 0.1 seconds missing
-        # new record will be an average position and time
+    # check difference in times of frame x and x+1
+    # if time difference is greater than 0.15 seconds
+    # make a new record for every 0.1 seconds missing
+    # new record will be an average position and time
 
-def visualizeFish(display_width,display_height):
+
+def visualizeFish(display_width, display_height):
+    # image from:
+    # https://www.zebrafishfilm.org/
+
     pygame.init()
 
     black = (0, 0, 0)
-    white = (255, 255, 255)
 
+    # set window size and caption it
     gameDisplay = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption('ZebraFish Visual')
 
-    fishImg = pygame.image.load('zebra_fish.jpg')
+    # load the image
+    fishImg = pygame.image.load('zebrafish.png')
 
     def fish(x, y):
         gameDisplay.blit(fishImg, (x, y))
@@ -54,19 +63,38 @@ def visualizeFish(display_width,display_height):
     clock = pygame.time.Clock()
     crashed = False
 
-    x = (display_width * 0.45)
-    y = (display_height * 0.8)
+    # copy the csv to a use-able format
+    new_rows_list = []
+
+    # open and read the data from the CSV
+    with open('fishDataCorrected.csv', newline='') as csvFile:
+        reader = csv.DictReader(csvFile)
+        for row in reader:
+            new_rows_list.append(row)
+
+    # track the frame count
+    frameCount = 0
+
+    # total number of frames we need
+    totalFrames = len(new_rows_list)
 
     while not crashed:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 crashed = True
 
-        gameDisplay.fill(white)
-        fish(x, y)
+        gameDisplay.fill(black)
+        print(frameCount)
+        fish(round(float(new_rows_list[frameCount].get("x-position"))), round(float(new_rows_list[frameCount].get("y-position"))))
+
+        # run until we run out of frames
+        if frameCount < totalFrames - 1:
+            frameCount = frameCount + 1
+        else:
+            frameCount = 0
 
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(10)
 
     pygame.quit()
     quit()
--- visualizeCSV/visualFishMovement.py
+++ visualizeCSV/visualFishMovement.py
@@ -1,3 +1,3 @@
 import functions
 
-functions.visualizeFish(500, 500)
+functions.visualizeFish(1000, 1000)
